74fe49bb7e078090deb070d519d613900f999192
Эшмуродов Азизбек <84049011+aeshmurodov@users.noreply.github.com>
2025-02-20 17:08:13 +0300

    Update vite.config.js for vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/learn_it/", // Указываем путь к репозиторию
+    base: "/", // Указываем путь к репозиторию
     plugins: [react()],
   });
   
